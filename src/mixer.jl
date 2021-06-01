c7d6bea922406c3f332ddaca96f6715dffe2b5c1
Li Huang <lihuang.dmft@gmail.com>
2021-06-01 21:07:45 +0800

    update mixer_delta()

--- src/mixer.jl
+++ src/mixer.jl
@@ -49,6 +49,11 @@ function mixer_delta(it::IterInfo, ai::Array{Impurity,1})
     # Check whether these files are available
     @assert isfile(fcurr) && isfile(fprev)
 
+    # Read in the local impurity levels (previous and current)
+    Ecurr = read_eimpx(ai, fcurr)
+    Eprev = read_eimpx(ai, fprev)
+    @assert size(Ecurr) == size(Eprev)
+
     # Print blank line for better visualization
     println()
 end
