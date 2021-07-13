496264c4b26ec244745b4a4471f4ceaee696cf5c
Li Huang <lihuang.dmft@gmail.com>
2021-07-14 01:16:11 +0800

    update version string

--- src/global.jl
+++ src/global.jl
@@ -81,7 +81,7 @@ Version of this julia package.
 
 See also: [`__RELEASE__`](@ref).
 """
-const __VERSION__ = v"0.5.6-devel.210713"
+const __VERSION__ = v"0.5.7-devel.210714"
 
 """
     __RELEASE__
