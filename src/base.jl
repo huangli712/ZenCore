72c7421d957c37ef5b90e3f94eb980422b7d1e9f
Li Huang <lihuang.dmft@gmail.com>
2021-07-17 23:39:26 +0800

    fix output string

--- src/base.jl
+++ src/base.jl
@@ -1448,9 +1448,9 @@ function show_it(mode::String, iter::I64, max_iter::I64)
     @assert mode in ("dmft1", "dmft2", "dft")
     @assert iter ≥ 1
     @assert max_iter ≥ iter
-    print("Mode : [ $mode ]. ")
-    print("Requested Iteration : [ $max_iter ]. ")
-    println("Finished Iteration: [ $iter ] \n")
+    print("Mode : [ $mode ], ")
+    print("Requested Iteration : [ $max_iter ], ")
+    println("Finished Iteration: [ $iter ]. \n")
 end
 
 """
