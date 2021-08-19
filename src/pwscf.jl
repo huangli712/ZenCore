760539dda9b85d5e33b7f450b5612a30fd2fba46
Li Huang <lihuang.dmft@gmail.com>
2021-08-19 16:05:46 +0800

    update pwscf.jl

--- src/pwscf.jl
+++ src/pwscf.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (lihuang.dmft@gmail.com)
 # Status  : Unstable
 #
-# Last modified: 2021/08/18
+# Last modified: 2021/08/19
 #
 
 function pwscf_adaptor()
@@ -12,38 +12,6 @@ end
 
 function pwscf_parser()
 end
-
-const Maybe{T} = Union{T,Nothing}
-
-"""
-    Input
-
-An abstract type representing an input object of ab initio software.
-All other input types should subtype `Input`.
-"""
-abstract type Input end
-
-"""
-    InputEntry
-
-Represent any component of an `Input`. The fields of an `Input` should
-all be either `InputEntry` or `Nothing` (no value provided).
-"""
-abstract type InputEntry end
-
-"""
-    Namelist <: InputEntry
-
-Represent a component of an `Input`, a basic Fortran data structure.
-"""
-abstract type Namelist <: InputEntry end
-
-"""
-    Card <: InputEntry
-
-Represent cards of an `Input` in Quantum ESPRESSO.
-"""
-abstract type Card <: InputEntry end
 
 export ControlNL
 
