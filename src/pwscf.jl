c7d2c51d4409cac943d1580336c3dcaf6bd87538
Li Huang <lihuang.dmft@gmail.com>
2021-08-20 11:40:58 +0800

    fix K_POINTS

--- src/pwscf.jl
+++ src/pwscf.jl
@@ -82,6 +82,152 @@ function process_namelists!(nml::Vector{Any}, keylist::Tuple)
     return NLData
 end
 
+
+#=
+### *Customized Structs*
+=#
+
+"""
+    ReciprocalPoint(x, y, z, w)
+Represent a special point of the 3D Brillouin zone. Each of them has a weight `w`.
+"""
+struct ReciprocalPoint{T}
+    coord :: Vector{T}
+    weight:: Float64
+end
+
+"""
+    MonkhorstPackGrid(mesh, is_shift)
+
+Represent the Monkhorst--Pack grid.
+
+### Arguments
+- `mesh`: A length-three vector specifying the k-point grid (``nk_1 × nk_2 × nk_3``) as in Monkhorst--Pack grids.
+- `is_shift`: A length-three vector specifying whether the grid is displaced by half a grid step in the corresponding directions.
+"""
+struct MonkhorstPackGrid
+    mesh::Vector{I64}
+    is_shift::Vector{Bool}
+    function MonkhorstPackGrid(mesh, is_shift)
+        @assert all(mesh .>= 1)
+        if eltype(is_shift) != Bool
+            is_shift = Bool.(is_shift)
+        end
+        return new(mesh, is_shift)
+    end
+end
+
+"""
+    AtomicSpecies(atom::Union{AbstractChar,String}, mass::Float64, pseudopot::String)
+    AtomicSpecies(x::AtomicPosition, mass, pseudopot)
+
+Represent each line of the `ATOMIC_SPECIES` card in QE.
+The `atom` field accepts at most 3 characters.
+
+### Examples
+```jldoctest
+julia> using QuantumESPRESSOBase.Cards.PWscf
+julia> AtomicSpecies("C1", 12, "C.pbe-n-kjpaw_psl.1.0.0.UPF")
+AtomicSpecies("C1", 12.0, "C.pbe-n-kjpaw_psl.1.0.0.UPF")
+julia> AtomicSpecies(
+           AtomicPosition('S', [0.500000000, 0.288675130, 1.974192764]),
+           32.066,
+           "S.pz-n-rrkjus_psl.0.1.UPF",
+       )
+AtomicSpecies("S", 32.066, "S.pz-n-rrkjus_psl.0.1.UPF")
+```
+"""
+struct AtomicSpecies
+    "Label of the atom. Max total length cannot exceed 3 characters."
+    atom :: String
+    """
+    Mass of the atomic species in atomic unit.
+    Used only when performing molecular dynamics (MD) run
+    or structural optimization runs using damped MD.
+    Not actually used in all other cases (but stored
+    in data files, so phonon calculations will use
+    these values unless other values are provided).
+    """
+    mass :: Float64
+    """
+    File containing pseudopotential for this species.
+    See also: [`pseudoformat`](@ref)
+    """
+    pseudopot::String
+    function AtomicSpecies(atom::Union{AbstractChar,AbstractString}, mass, pseudopot)
+        @assert length(atom) <= 3 "`atom` can have at most 3 characters!"
+        return new(string(atom), mass, pseudopot)
+    end
+end
+
+"""
+    AtomicPosition(atom::Union{AbstractChar,String}, pos::Vector{Float64}[, if_pos::Vector{Int}])
+    AtomicPosition(x::AtomicSpecies, pos, if_pos)
+
+Represent each line of the `ATOMIC_POSITIONS` card in QE.
+The `atom` field accepts at most 3 characters.
+
+### Examples
+```jldoctest
+julia> using QuantumESPRESSOBase.Cards.PWscf
+julia> AtomicPosition('O', [0, 0, 0])
+AtomicPosition("O", [0.0, 0.0, 0.0], Bool[1, 1, 1])
+julia> AtomicPosition(
+           AtomicSpecies('S', 32.066, "S.pz-n-rrkjus_psl.0.1.UPF"),
+           [0.500000000, 0.288675130, 1.974192764],
+       )
+AtomicPosition("S", [0.5, 0.28867513, 1.974192764], Bool[1, 1, 1])
+```
+"""
+struct AtomicPosition
+    "Label of the atom as specified in `AtomicSpecies`."
+    atom::String
+    "Atomic positions. A three-element vector of floats."
+    pos::Vector{Float64}
+    """
+    Component `i` of the force for this atom is multiplied by `if_pos(i)`,
+    which must be either `0` or `1`.  Used to keep selected atoms and/or
+    selected components fixed in MD dynamics or structural optimization run.
+    With `crystal_sg` atomic coordinates the constraints are copied in all equivalent
+    atoms.
+    """
+    if_pos::Vector{Bool}
+    function AtomicPosition(atom::Union{AbstractChar,AbstractString}, pos, if_pos)
+        @assert length(atom) <= 3 "`atom` can have at most 3 characters!"
+        return new(string(atom), pos, if_pos)
+    end
+end
+
+abstract type Card end
+
+"""
+    AtomicSpeciesCard <: Card
+Represent the `ATOMIC_SPECIES` card in QE. It does not have an "option".
+"""
+struct AtomicSpeciesCard <: Card
+    data::Vector{AtomicSpecies}
+end
+
+"""
+    AtomicPositionsCard <: Card
+
+Represent the `ATOMIC_POSITIONS` card in QE.
+
+### Arguments
+- `data::AbstractVector{AtomicPosition}`: A vector containing `AtomicPosition`s.
+- `option::String="alat"`: allowed values are: "alat", "bohr", "angstrom", "crystal", and "crystal_sg".
+"""
+struct AtomicPositionsCard <: Card
+    data::Vector{AtomicPosition}
+    option::String
+    function AtomicPositionsCard(data, option = "alat")
+        @assert option in optionpool(AtomicPositionsCard)
+        return new(data, option)
+    end
+end
+
+
+
 mutable struct PWInput
     ControlNL
     SystemNL
@@ -287,56 +433,6 @@ const ATOMIC_SPECIES_ITEM = r"""
 """mx
 
 
-"""
-    AtomicSpecies(atom::Union{AbstractChar,String}, mass::Float64, pseudopot::String)
-    AtomicSpecies(x::AtomicPosition, mass, pseudopot)
-Represent each line of the `ATOMIC_SPECIES` card in QE.
-The `atom` field accepts at most 3 characters.
-# Examples
-```jldoctest
-julia> using QuantumESPRESSOBase.Cards.PWscf
-julia> AtomicSpecies("C1", 12, "C.pbe-n-kjpaw_psl.1.0.0.UPF")
-AtomicSpecies("C1", 12.0, "C.pbe-n-kjpaw_psl.1.0.0.UPF")
-julia> AtomicSpecies(
-           AtomicPosition('S', [0.500000000, 0.288675130, 1.974192764]),
-           32.066,
-           "S.pz-n-rrkjus_psl.0.1.UPF",
-       )
-AtomicSpecies("S", 32.066, "S.pz-n-rrkjus_psl.0.1.UPF")
-```
-"""
-struct AtomicSpecies
-    "Label of the atom. Max total length cannot exceed 3 characters."
-    atom :: String
-    """
-    Mass of the atomic species in atomic unit.
-    Used only when performing molecular dynamics (MD) run
-    or structural optimization runs using damped MD.
-    Not actually used in all other cases (but stored
-    in data files, so phonon calculations will use
-    these values unless other values are provided).
-    """
-    mass :: Float64
-    """
-    File containing pseudopotential for this species.
-    See also: [`pseudoformat`](@ref)
-    """
-    pseudopot::String
-    function AtomicSpecies(atom::Union{AbstractChar,AbstractString}, mass, pseudopot)
-        @assert length(atom) <= 3 "`atom` can have at most 3 characters!"
-        return new(string(atom), mass, pseudopot)
-    end
-end
-
-abstract type Card end
-
-"""
-    AtomicSpeciesCard <: Card
-Represent the `ATOMIC_SPECIES` card in QE. It does not have an "option".
-"""
-struct AtomicSpeciesCard <: Card
-    data::Vector{AtomicSpecies}
-end
 
 export AtomicSpeciesCard
 export AtomicSpecies
@@ -357,61 +453,10 @@ function Base.tryparse(::Type{AtomicSpeciesCard}, str::AbstractString)
     end
 end # function Base.tryparse
 
-"""
-    AtomicPosition(atom::Union{AbstractChar,String}, pos::Vector{Float64}[, if_pos::Vector{Int}])
-    AtomicPosition(x::AtomicSpecies, pos, if_pos)
-Represent each line of the `ATOMIC_POSITIONS` card in QE.
-The `atom` field accepts at most 3 characters.
-# Examples
-```jldoctest
-julia> using QuantumESPRESSOBase.Cards.PWscf
-julia> AtomicPosition('O', [0, 0, 0])
-AtomicPosition("O", [0.0, 0.0, 0.0], Bool[1, 1, 1])
-julia> AtomicPosition(
-           AtomicSpecies('S', 32.066, "S.pz-n-rrkjus_psl.0.1.UPF"),
-           [0.500000000, 0.288675130, 1.974192764],
-       )
-AtomicPosition("S", [0.5, 0.28867513, 1.974192764], Bool[1, 1, 1])
-```
-"""
-struct AtomicPosition
-    "Label of the atom as specified in `AtomicSpecies`."
-    atom::String
-    "Atomic positions. A three-element vector of floats."
-    pos::Vector{Float64}
-    """
-    Component `i` of the force for this atom is multiplied by `if_pos(i)`,
-    which must be either `0` or `1`.  Used to keep selected atoms and/or
-    selected components fixed in MD dynamics or structural optimization run.
-    With `crystal_sg` atomic coordinates the constraints are copied in all equivalent
-    atoms.
-    """
-    if_pos::Vector{Bool}
-    function AtomicPosition(atom::Union{AbstractChar,AbstractString}, pos, if_pos)
-        @assert length(atom) <= 3 "`atom` can have at most 3 characters!"
-        return new(string(atom), pos, if_pos)
-    end
-end
 AtomicPosition(atom, pos) = AtomicPosition(atom, pos, trues(3))
 AtomicPosition(x::AtomicSpecies, pos, if_pos) = AtomicPosition(x.atom, pos, if_pos)
 # Introudce mutual constructors since they share the same atoms.
 AtomicSpecies(x::AtomicPosition, mass, pseudopot) = AtomicSpecies(x.atom, mass, pseudopot)
-
-"""
-    AtomicPositionsCard <: Card
-Represent the `ATOMIC_POSITIONS` card in QE.
-# Arguments
-- `data::AbstractVector{AtomicPosition}`: A vector containing `AtomicPosition`s.
-- `option::String="alat"`: allowed values are: "alat", "bohr", "angstrom", "crystal", and "crystal_sg".
-"""
-struct AtomicPositionsCard <: Card
-    data::Vector{AtomicPosition}
-    option::String
-    function AtomicPositionsCard(data, option = "alat")
-        @assert option in optionpool(AtomicPositionsCard)
-        return new(data, option)
-    end
-end
 
 optionpool(::Type{AtomicPositionsCard}) =
     ("alat", "bohr", "angstrom", "crystal", "crystal_sg")
@@ -517,34 +562,6 @@ function Base.tryparse(::Type{AtomicPositionsCard}, str::AbstractString)
         )
     end
 end # function Base.tryparse
-
-"""
-    ReciprocalPoint(x, y, z, w)
-Represent a special point of the 3D Brillouin zone. Each of them has a weight `w`.
-"""
-struct ReciprocalPoint{T}
-    coord :: Vector{T}
-    weight:: Float64
-end
-
-"""
-    MonkhorstPackGrid(mesh, is_shift)
-Represent the Monkhorst--Pack grid.
-# Arguments
-- `mesh`: A length-three vector specifying the k-point grid (``nk_1 × nk_2 × nk_3``) as in Monkhorst--Pack grids.
-- `is_shift`: A length-three vector specifying whether the grid is displaced by half a grid step in the corresponding directions.
-"""
-struct MonkhorstPackGrid
-    mesh::Vector{UInt}
-    is_shift::Vector{Bool}
-    function MonkhorstPackGrid(mesh, is_shift)
-        @assert all(mesh .>= 1)
-        if eltype(is_shift) != Bool
-            is_shift = Bool.(is_shift)
-        end
-        return new(mesh, is_shift)
-    end
-end
 
 abstract type KPointsCard <: Card end
 
