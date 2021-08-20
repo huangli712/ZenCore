ef67d0d5436349553730cb3f8b18f7561b0b5048
Li Huang <lihuang.dmft@gmail.com>
2021-08-20 12:00:54 +0800

    update code style

--- src/pwscf.jl
+++ src/pwscf.jl
@@ -101,23 +101,34 @@ struct ReciprocalPoint{T}
 end
 
 """
-    MonkhorstPackGrid(mesh, is_shift)
+    MonkhorstPackGrid
 
-Represent the Monkhorst--Pack grid.
+Represent the Monkhorst-Pack grid.
 
-### Arguments
-- `mesh`: A length-three vector specifying the k-point grid (``nk_1 × nk_2 × nk_3``) as in Monkhorst--Pack grids.
-- `is_shift`: A length-three vector specifying whether the grid is displaced by half a grid step in the corresponding directions.
+### Members
+
+* mesh  -> A length-three vector specifying the ``k``-point grid
+           (``nk_1 × nk_2 × nk_3``) as in Monkhorst-Pack grids.
+* shift -> A length-three vector specifying whether the grid is displaced
+           by half a grid step in the corresponding directions.
+
+See also: [`ReciprocalPoint`](@ref).
 """
 struct MonkhorstPackGrid
-    mesh::Vector{I64}
-    is_shift::Vector{Bool}
-    function MonkhorstPackGrid(mesh, is_shift)
+    mesh  :: Vector{I64}
+    shift :: Vector{Bool}
+
+    # Inner constructor
+    function MonkhorstPackGrid(mesh, shift)
+        @assert length(mesh) == 3
+        @assert length(shift) == 3
         @assert all(mesh .>= 1)
-        if eltype(is_shift) != Bool
-            is_shift = Bool.(is_shift)
+        #
+        if eltype(shift) != Bool
+            shift = Bool.(shift)
         end
-        return new(mesh, is_shift)
+        #
+        return new(mesh, shift)
     end
 end
 
