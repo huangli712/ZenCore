1b0368706333d2a74c8a2c0a0fa4ebb08ae7724e
Li Huang <lihuang.dmft@gmail.com>
2021-08-30 00:11:42 +0800

    add wannier_init()

--- src/wannier.jl
+++ src/wannier.jl
@@ -4,8 +4,38 @@
 # Author  : Li Huang (lihuang.dmft@gmail.com)
 # Status  : Unstable
 #
-# Last modified: 2021/08/15
+# Last modified: 2021/08/30
 #
 
+#=
+### *Driver Functions*
+=#
+
 function wannier_adaptor()
+end
+
+#=
+### *Service Functions* : *Group A*
+=#
+
+function wannier_init()
+end
+
+function wannier_exec()
+end
+
+function wannier_save()
+end
+
+#=
+### *Service Functions* : *Group X*
+=#
+
+function pw2wan_init()
+end
+
+function pw2wan_exec()
+end
+
+function pw2wan_save()
 end
