a4c6cdc1c84fd7d466a2d4d34a86f7e873da5720
Li Huang <lihuang.dmft@gmail.com>
2021-10-29 14:49:06 +0800

    update version string

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This package is currently under developement. **IT DOESN'T WORK NOW!**
 
 ## Version
 
-v0.7.39-devel.211026
+v0.7.40-devel.211029
 
 ## License
 
--- src/global.jl
+++ src/global.jl
@@ -121,7 +121,7 @@ Version of this julia package.
 
 See also: [`__RELEASE__`](@ref).
 """
-const __VERSION__ = v"0.7.39-devel.211026"
+const __VERSION__ = v"0.7.40-devel.211029"
 
 """
     __RELEASE__
