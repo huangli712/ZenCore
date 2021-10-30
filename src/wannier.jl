5d8b10deda58135d04c50cc1ca5b3faed780264c
Li Huang <lihuang.dmft@gmail.com>
2021-10-30 22:07:51 +0800

    add comments

--- src/wannier.jl
+++ src/wannier.jl
@@ -1987,7 +1987,9 @@ end
                   kend::Array{F64,2})
 
 Try to generate 𝑘-path along the selected high-symmetry directions in
-the Brillouin zone.
+the Brillouin zone. The argument `ndiv` means the number of divisions
+for each 𝑘-path. While `kstart` and `kend` denote the 𝑘 coordinates for
+the starting and ending points of the 𝑘-path, respectively.
 """
 function w90_make_path(ndiv::I64,
                        kstart::Array{F64,2},
