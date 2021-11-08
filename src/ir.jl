3a70068e307a5b623a9d8b713fd0f62fdc20818d
Li Huang <lihuang.dmft@gmail.com>
2021-11-08 10:38:43 +0800

    fix ir_read()

--- src/ir.jl
+++ src/ir.jl
@@ -878,37 +878,35 @@ directory that this file exists.
 function irio_tetra(f::String)
     # Check file's status
     fn = joinpath(f, "tetra.ir")
+    @assert isfile(fn)
 
     # Define tetrahedra
     volt = 0.0
     itet = nothing
 
     # Input the data
-    # If tetra.ir does not exist, we just return nothing.
-    if isfile(fn)
-        open(fn, "r") do fin
-            # Skip the header
-            readline(fin)
-            readline(fin)
-            readline(fin)
+    open(fn, "r") do fin
+        # Skip the header
+        readline(fin)
+        readline(fin)
+        readline(fin)
 
-            # Extract some dimensional parameters
-            ntet = parse(I64, line_to_array(fin)[3])
-            volt = parse(F64, line_to_array(fin)[3])
-            readline(fin)
+        # Extract some dimensional parameters
+        ntet = parse(I64, line_to_array(fin)[3])
+        volt = parse(F64, line_to_array(fin)[3])
+        readline(fin)
 
-            # Allocate memory
-            itet = zeros(I64, ntet, 5)
+        # Allocate memory
+        itet = zeros(I64, ntet, 5)
 
-            # Get the tetrahedra
-            for t = 1:ntet
-                itet[t, :] = parse.(I64, line_to_array(fin))
-            end
-        end # END OF IOSTREAM
+        # Get the tetrahedra
+        for t = 1:ntet
+            itet[t, :] = parse.(I64, line_to_array(fin))
+        end
+    end # END OF IOSTREAM
 
-        # Print some useful information
-        println("  > Open and parse the file tetra.ir (itet and volt)")
-    end
+    # Print some useful information
+    println("  > Open and parse the file tetra.ir (itet and volt)")
 
     # Return the desired arrays
     return volt, itet
